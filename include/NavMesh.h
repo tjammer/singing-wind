8d747e687ee957560d0c11355d8f390292e53da3
Tobias Jammer <jammer@thinkpad>
2017-06-02 13:40:46 +0200

    started navmesh

--- include/NavMesh.h
+++ include/NavMesh.h
@@ -5,10 +5,84 @@
 #ifndef SINGING_WIND_NAVMESH_H
 #define SINGING_WIND_NAVMESH_H
 
+#include "Island.h"
+#include "ColGrid.h"
+#define GLM_ENABLE_EXPERIMENTAL
+#include <glm/gtx/hash.hpp>
+#include <nanoflann.hpp>
 
-class NavMesh {
-
+enum class LinkType : int {
+    Walk,
+    Jump,
+    Fall
 };
+
+using NavNode = glm::tvec2<int>;
+
+struct NavLink {
+    NavNode first;
+    NavNode second;
+    float weight;
+    LinkType link_type = LinkType::Walk;
+    NavNode& get_next(const NavNode& n);
+
+    NavLink(const NavNode& first, const NavNode& second);
+};
+
+// spatial partitioing to find nearest node
+
+
+struct NavTree {
+    NavTree();
+    ~NavTree() = default;
+
+    void rebuild(const std::vector<NavNode>& nodes);
+    NavNode & get_nearest(const WVec &pos);
+private:
+    struct NavCloud {
+        std::vector<NavNode> nodes;
+
+        // Must return the number of data points
+        inline size_t kdtree_get_point_count() const { return nodes.size(); }
+
+        // Returns the distance between the vector "p1[0:size-1]" and the data point with index "idx_p2" stored in the class:
+        inline float kdtree_distance(const float *p1, const size_t idx_p2, size_t /*size*/) const
+        {
+            const float d0=p1[0]-nodes[idx_p2].x;
+            const float d1=p1[1]-nodes[idx_p2].y;
+            return d0*d0+d1*d1;
+        }
+
+        // Returns the dim'th component of the idx'th point in the class:
+        // Since this is inlined and the "dim" argument is typically an immediate value, the
+        //  "if/else's" are actually solved at compile time.
+        inline float kdtree_get_pt(const size_t idx, int dim) const
+        {
+            if (dim==0) return nodes[idx].x;
+            else return nodes[idx].y;
+        }
+
+        // Optional bounding-box computation: return false to default to a standard bbox computation loop.
+        //   Return true if the BBOX was already computed by the class and returned in "bb" so it can be avoided to redo it again.
+        //   Look at bb.size() to find out the expected dimensionality (e.g. 2 or 3 for point clouds)
+        template <class BBOX>
+        bool kdtree_get_bbox(BBOX& /*bb*/) const { return false; }
+    };
+    using KDTree = nanoflann::KDTreeSingleIndexAdaptor<nanoflann::L1_Adaptor<float, NavCloud> , NavCloud, 2>;
+    NavCloud m_cloud;
+    KDTree m_kd_tree;
+};
+
+struct NavMesh {
+    std::unordered_map<NavNode, std::vector<NavLink>> graph;
+    void build_tree();
+
+    NavNode get_nearest(const WVec &pos);
+private:
+    NavTree m_tree;
+};
+
+void build_navmesh(const std::vector<Island> &m_islands, const StaticGrid &grid, const WVec &mouse);
 
 
 #endif //SINGING_WIND_NAVMESH_H
--- src/NavMesh.cpp
+++ src/NavMesh.cpp
@@ -12,7 +12,7 @@
 #include <glm/gtx/matrix_transform_2d.hpp>
 
 std::unordered_map<NavNode, std::vector<NavLink>> walkable_from_tri(std::array<WVec, 3> tri, const StaticGrid &grid) {
-    std::unordered_map<NavNode, std::vector<NavLink>> links;
+    std::unordered_map<NavNode, std::vector<NavLink>> graph;
     std::vector<NavNode> nodes;
     for (auto i = 0 ; i < 3 ; ++i) {
         auto norm_tang = w_normalize(w_tangent(tri[i] - tri[(i+1)%3]));
@@ -31,17 +31,16 @@ std::unordered_map<NavNode, std::vector<NavLink>> walkable_from_tri(std::array<W
             if (!result.collides) {
                 NavNode n1 = {tri[i].x, tri[i].y - 15};
                 NavNode n2 = {tri[(i+1)%3].x, tri[(i+1)%3].y - 15};
-                NavLink nl;
-                nl.nodes = {n1, n2};
-                links[n1].push_back(nl);
-                links[n2].push_back(nl);
+                NavLink nl = {n1, n2};
+                graph[n1].push_back(nl);
+                graph[n2].push_back(nl);
             }
         }
     }
-    return links;
+    return graph;
 }
 
-void build_navmesh(const std::vector<Island> &m_islands, const StaticGrid &grid) {
+void build_navmesh(const std::vector<Island> &m_islands, const StaticGrid &grid, const WVec &mouse) {
     NavMesh mesh;
     std::vector<WVec> triangles;
     for (const auto &island : m_islands) {
@@ -54,23 +53,78 @@ void build_navmesh(const std::vector<Island> &m_islands, const StaticGrid &grid)
             for (const auto &pr : links) {
                 auto node = pr.first;
                 for (const auto &link : pr.second) {
-                    mesh.links[node].push_back(link);
+                    mesh.graph[node].push_back(link);
                 }
             }
         }
     }
+    mesh.build_tree();
     // render
-    for (const auto &node : mesh.links) {
+    for (const auto &pr : mesh.graph) {
+        auto & node = pr.first;
+        auto & links = pr.second;
         WRenderer::set_mode(GL_QUADS);
-        auto quad = make_quad({node.first.x, node.first.y}, 5);
-        for (const auto &q : quad) {
+        for (const auto &q : make_quad({node.x, node.y}, 5)) {
             WRenderer::add_primitive_vertex({{q.x, q.y}, {0, 1, 1}});
         }
         WRenderer::set_mode(GL_LINES);
         // links
-        for (const auto & link : node.second) {
-            WRenderer::add_primitive_vertex({{(float)link.nodes.first.x, (float)link.nodes.first.y}, {1, 1, 0}});
-            WRenderer::add_primitive_vertex({{(float)link.nodes.second.x, (float)link.nodes.second.y}, {1, 1, 0}});
+        for (const auto & link : links) {
+            WRenderer::add_primitive_vertex({{(float)link.first.x, (float)link.first.y}, {1, 1, 0}});
+            WRenderer::add_primitive_vertex({{(float)link.second.x, (float)link.second.y}, {1, 1, 0}});
         }
     }
+    const auto & node = mesh.get_nearest(mouse);
+    WRenderer::set_mode(GL_QUADS);
+    for (const auto & q : make_quad({node.x, node.y}, 6)) {
+        WRenderer::add_primitive_vertex({{q.x, q.y}, {1, 0, 1}});
+    }
+}
+
+NavLink::NavLink(const NavNode &first, const NavNode &second)
+    : first(first), second(second) {
+    weight = w_magnitude(first - second);
+}
+
+NavNode &NavLink::get_next(const NavNode &n) {
+    if (n == first) {
+        return second;
+    }
+    else if (n == second) {
+        return first;
+    }
+    assert(false);
+    return first;
+}
+
+
+NavTree::NavTree() : m_cloud(), m_kd_tree(2, m_cloud, nanoflann::KDTreeSingleIndexAdaptorParams(5)) {
+}
+
+void NavTree::rebuild(const std::vector<NavNode> &nodes) {
+    m_cloud.nodes = nodes;
+    m_kd_tree.buildIndex();
+}
+
+NavNode & NavTree::get_nearest(const WVec &pos) {
+    float query_pt[2] = {pos.x, pos.y};
+
+    std::vector<size_t>   ret_index(1);
+    std::vector<float> out_dist_sqr(1);
+
+    m_kd_tree.knnSearch(&query_pt[0], 1, &ret_index[0], &out_dist_sqr[0]);
+    return m_cloud.nodes[ret_index[0]];
+}
+
+void NavMesh::build_tree() {
+    std::vector<NavNode> nodes;
+    for (auto &pr : graph) {
+        auto & node = pr.first;
+        nodes.push_back(node);
+    }
+    m_tree.rebuild(nodes);
+}
+
+NavNode NavMesh::get_nearest(const WVec &pos) {
+    return m_tree.get_nearest(pos);
 }
