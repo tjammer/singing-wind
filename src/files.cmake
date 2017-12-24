6bc95ebb63efdc3dba92d5ccabef2e82e3ba1491
Tobias Jammer <tobiasjammer@googlemail.com>
2017-12-24 11:54:10 +0100

    restrcutered again

--- src/files.cmake
+++ src/files.cmake
@@ -1,16 +1,50 @@
-include_directories(
-    ${CMAKE_CURRENT_LIST_DIR}/engine
-    ${CMAKE_CURRENT_LIST_DIR}/skills
-    ${CMAKE_CURRENT_LIST_DIR}/behaviours
-    )
-
-include(${CMAKE_CURRENT_LIST_DIR}/engine/engine.cmake)
-include(${CMAKE_CURRENT_LIST_DIR}/skills/skills.cmake)
-include(${CMAKE_CURRENT_LIST_DIR}/behaviours/behaviours.cmake)
 include(${CMAKE_CURRENT_LIST_DIR}/schemas/schemas.cmake)
 
 set(SOURCE_FILES
-    ${ENGINE_SOURCE}
-    ${SKILLS_SOURCE}
-    ${BEHAVIOURS_SOURCE}
+    src/Game.cpp
+    src/main.cpp
+    src/Actor.cpp
+    src/BCurve.cpp
+    src/Camera.cpp
+    src/Editor.cpp
+    src/Engine.cpp
+    src/Island.cpp
+    src/WInput.cpp
+    src/NavMesh.cpp
+    src/SceneIO.cpp
+    src/systems.cpp
+    src/WShader.cpp
+    src/ColShape.cpp
+    src/entities.cpp
+    src/steering.cpp
+    src/WTexture.cpp
+    src/Collision.cpp
+    src/GameWorld.cpp
+    src/WRenderer.cpp
+    src/Components.cpp
+    src/EditorDraw.cpp
+    src/FrameTimer.cpp
+    src/CPruneSweep.cpp
+    src/MoveSystems.cpp
+    src/Pathfinding.cpp
+    src/Protagonist.cpp
+    src/SimpleFlyer.cpp
+    src/triangulate.cpp
+    src/EditorStates.cpp
+    src/EntityEditor.cpp
+    src/PosComponent.cpp
+    src/TagComponent.cpp
+    src/ai_input_funcs.cpp
+    src/ai_state_funcs.cpp
+    src/SkillComponent.cpp
+    src/HurtBoxComponent.cpp
+    src/CollisionComponent.cpp
+    src/imgui_impl_glfw_gl3.cpp
+    src/ai_state_transitions.cpp
+    src/StatusEffectComponent.cpp
+    src/PatrolComponent.cpp
+    src/AIComponent.cpp
+    src/Melee.cpp
+    src/Lounge.cpp
+    src/NTimes.cpp
     )
--- src/skills/skills.cmake
+++
@@ -1,7 +0,0 @@
-include_directories(
-    ${CMAKE_CURRENT_LIST_DIR})
-
-set(SKILLS_SOURCE
-    ${CMAKE_CURRENT_LIST_DIR}/Melee.cpp
-    ${CMAKE_CURRENT_LIST_DIR}/Lounge.cpp
-    )
